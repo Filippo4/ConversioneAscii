e1b47d7a88df5f2d5ff184e4614215c7f9b64ceb
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-12 11:40:45 +0200

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Parola da convertire");
+            string g = Console.ReadLine();
+            foreach (char c in g)
+            {
+                Console.WriteLine(System.Convert.ToInt32(c));
+            }
+            Console.ReadKey();
+        }
+    }
+}
